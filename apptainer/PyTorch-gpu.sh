8046fe515b669e460af8dc1d9d4d85b830324989
Chris Hill <cnh@mit.edu>
2023-06-16 21:32:57 -0400

    Update PyTorch-gpu.sh

--- apptainer/PyTorch-gpu.sh
+++ apptainer/PyTorch-gpu.sh
@@ -7,5 +7,5 @@ cd ${wdir}
 export APPTAINER_CACHEDIR=/tmp/cnh/apptainer-temporary-cache
 apptainer pull docker://nvcr.io/nvidia/pytorch:23.05-py3
 
-apptainer run --nv foo.sif /bin/bash
+apptainer run --nv pytorch_23.05-py3.sif /bin/bash
 
