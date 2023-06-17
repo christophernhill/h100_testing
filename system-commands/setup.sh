861c1408339f10aaa3a39c935900ade6c4ca8f55
Chris Hill <cnh@mit.edu>
2023-06-17 13:03:15 -0400

    Update setup.sh

--- system-commands/setup.sh
+++ system-commands/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+dnf -Y install apptainer
+dnf -y group install "Development Tools"
 dnf -y install nvidia-fabric-manager-530.30.02
 systemctl enable nvidia-fabricmanager
 systemctl start nvidia-fabricmanager
